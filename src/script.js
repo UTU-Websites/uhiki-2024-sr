88eb04752bfaaa9eb49e85e43f18c54ca5526a82
S-Qui <147270652+S-Qui@users.noreply.github.com>
2024-08-15 02:52:28 +0300

    tribute touch ups

--- src/script.js
+++ src/script.js
@@ -33,3 +33,29 @@ for (var i = 0; i < coll.length; i++) {
 }
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  var buttons = document.querySelectorAll(".ayaBtn");
+
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      var parentParagraph = this.closest('p'); // Find the closest parent <p> element
+      var dots = parentParagraph.querySelector(".dots");
+      var moreText = parentParagraph.querySelector(".more");
+      
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        this.innerHTML = "Read More"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        this.innerHTML = "Read Less"; 
+        moreText.style.display = "inline";
+      }
+    });
+  });
+});
+
+
+
+
+
